16830507d40948e40f14064708f7b2fb50c48bc9
chandragan22 <cgangavarapu22@gmail.com>
2019-01-02 14:33:01 -0600

    more functions

--- js/app.js
+++ js/app.js
@@ -5,14 +5,9 @@ var setup = {
           {title: 'Zuma Beach', location: {lat: 34.0218, lng: -118.8312}, type: 'Beach'},
           {title: 'Malibu Creek State Park', location: {lat: 34.0980, lng: -118.7314}, type: 'Park'},
           {title: 'Solstice Canyon', location: {lat: 34.0390, lng: -118.7552}, type: 'Canyon'},
-          {title: 'Escondido Falls', location: {lat: 34.0432, lng: -118.7795}, type: 'Waterfalls'},
-        ],
-    searchOptions: [
-        {name: 'Beach'},
-        {name: 'Canyon'},
-        {name: 'Waterfalls'},
-        {name: 'Park'}
-]};
+          {title: 'Santa Monica Mountains', location: {lat: 34.1203, lng: -118.9318}, type: 'Mountains'},
+        ]
+};
 var map;
 var marker;
 var markers = new Array();
@@ -24,53 +19,117 @@ function initMap() {
           mapTypeControl: false
         }); 
       
-        
-      }
-
+        google.maps.event.addListenerOnce(map, 'idle', function(){  //maps api will be loaded before viewmodel loads
+        ko.applyBindings(new ViewModel(setup));
+})
+};
     var ViewModel = function(data) {
         var self = this;
-        this.largeInfoWindow = new google.maps.InfoWindow();
+        var largeInfoWindow = new google.maps.InfoWindow();
         for (var i = 0; i < setup.locations.length; i++) {
           // Get the position from the location array.
           this.position = setup.locations[i].location;
           this.title = setup.locations[i].title;
           // Create a marker per location, and put into markers array.
-          marker = new google.maps.Marker({
+          var marker = new google.maps.Marker({
             position: this.position,
             map: map,
             title: this.title,
             animation: google.maps.Animation.DROP,
+            type: setup.locations[i].type,
             id: i
           });
           // Push the marker to our array of markers.
           markers.push(marker);
           // Create an onclick event to open an infowindow at each marker.
           marker.addListener('click', function() {
-            whenClicked()
+            populateInfoWindow(this, largeInfoWindow)
           });
-          bounds.extend(markers[i].position);
-            }
-            // Extend the boundaries of the map for each marker
-            map.fitBounds(bounds);
-      }
-      var whenClicked = function(event) {
-        populateInfoWindow(this, largeInfowindow);
-        google.maps.Animation.BOUNCE
-      }
+            };
+        
       // This function populates the infowindow when the marker is clicked. We'll only allow
       // one infowindow which will open at the marker that is clicked, and populate based
       // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('insert request');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
+        function populateInfoWindow(marker, infowindow) {
+            var self = this;
+            marker.setAnimation(google.maps.Animation.BOUNCE)
+            setTimeout(function(){ marker.setAnimation(null); }, 750);
+            // Check to make sure the infowindow is not already opened on this marker.
+            if (infowindow.marker != marker) {
+                infowindow.marker = marker;
+                var content = "<h4>Wikipedia links</h4>";
+                //Wikipedia request from Udacity course, changed a little bit to add more details
+                var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+                var wikiRequestTimeout = setTimeout(function(){
+                    infowindow.setContent("failed to get wikipedia resources"); //error handling
+                }, 600);
+                $.ajax({
+                    url: wikiUrl,
+                    dataType: "jsonp",
+                    jsonp: "callback",
+                    success: function(  response  ){
+                        var articleList = response[1];
+                        for (var i = 0; i < articleList.length; i++) {
+                            articleStr = articleList[i];
+                            url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                            //prints url and the first few words for readers
+                            content += '<li><a href="' + url + '">' + articleStr + '</a></li>' + response[2] + '...'  
+                            infowindow.setContent(content);
+                        };
+                            clearTimeout(wikiRequestTimeout);
+                        }
+                    });
+                infowindow.open(map, marker);
+                // Make sure the marker property is cleared if the infowindow is closed.
+                infowindow.addListener('closeclick',function(){
+                    infowindow.setMarker = null;
+                    });
+            }};
+
+        //function for the list of locations and filter function
+        //search options
+        options = ko.observableArray([
+                {type: 'None'},
+                {type: 'Beach'},
+                {type: 'Park'},
+                {type: 'Mountains'},
+                {type: 'Canyon'}
+            ]);
+        
+        self.markersList =  ko.observableArray();
+        self.selectedType = ko.observable('None');
+        self.filteredAction = ko.computed(function() {
+            if (self.selectedType() == "None") {
+                self.markersList = ko.observableArray(markers);
+                return self.markersList()
+            }
+            else {
+                for (var i = 0; i < markers.length; i++) {
+                    markers[i].setVisible(false);
+                    if (self.selectedType() == markers[i].type) {
+                        markers[i].setVisible(true);
+                        self.markersList.push(markers[i])
+                    return markers
+                    return self.markersList()
+                    }
+            }
+            }
+        })
+        console.log(self.selectedType())
+
+        //bounce and open infowindow when list view is clicked
+        listOpenWindow = function() {
+            console.log(this)
+            for (var i = 0; i < markers.length; i++) {
+            if (markers[i].title == this) {
+                populateInfoWindow(markers[i], largeInfoWindow)
+            }
+           } 
         }
+        
+
+
+
 };
 
-ko.applyBindings(new ViewModel(setup));
+
---
+++ js/app2.js
@@ -0,0 +1,10 @@
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 34.0259, lng: -118.7798},
+          zoom: 10,
+          mapTypeControl: false
+        });
+      
+var largeInfowindow = new google.maps.InfoWindow();  
+};
+
